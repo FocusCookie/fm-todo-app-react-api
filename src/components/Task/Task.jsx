64cb12e90ad133955918ab170f7d58b05aed8219
FocusCookie <stephan_lueddemann@web.de>
2021-10-04 20:37:40 +0200

    Refactored so that the input value is a state which can be updated wiithin the component itself. A parent can look for a change from the outside.

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -6,60 +6,81 @@ import checkIcon from "../../images/icon-check.svg";
 
 //TODO: Refactor this component into  a component class with state so to change the input on change, This is right now a functional component
 // https://reactjs.org/docs/forms.html
+/* //TODO: Checken ob die props sich aktuallisieren sprich ob sie neu gerendert werden bei einer änderung
+dies geschieht mit dem nested object in storybook nämlich nicht */
 
-export const Task = ({
-  task: { id, completed, description },
-  onCompleteTask,
-  onDeleteTask,
-  ...props
-}) => {
-  let checked;
-  if (completed) {
-    checked = (
-      <img
-        tabIndex="1"
-        src={checkIcon}
-        className="check--checked"
-        alt="Check a task as complete."
-      />
-    );
-  } else {
-    checked = <div tabIndex="1" className="check--unchecked"></div>;
+class Task extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  return (
-    <div class="task" {...props}>
-      <div
-        className={`checkbox ${completed ? "checkbox-bg" : ""}`}
-        onClick={(event) => onCompleteTask(id)}
-      >
-        {checked}
-      </div>
+  componentDidMount() {
+    this.setState({ value: this.props.task.description });
+  }
 
-      <input
-        type="text"
-        id="description"
-        className={`description ${completed ? "text--checked" : ""}`}
-        name="description"
-        defaultValue={description}
-      />
-      <div onClick={(event) => event.stopPropagation()}>
-        <button
-          className="delete"
-          onClick={() => {
-            onDeleteTask(id);
-          }}
-        >
-          <img src={crossIcon} className="delete-icon" alt="Delete task." />
-        </button>
-      </div>
-    </div>
-  );
-};
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    const renderTask = () => {
+      const { task, onCompleteTask, onDeleteTask } = this.props;
+
+      let checked;
+      if (task.completed) {
+        checked = (
+          <img
+            tabIndex="1"
+            src={checkIcon}
+            className="check--checked"
+            alt="Check a task as complete."
+          />
+        );
+      } else {
+        checked = <div tabIndex="1" className="check--unchecked"></div>;
+      }
+
+      return (
+        <div className="task">
+          <div
+            className={`checkbox ${task.completed ? "checkbox-bg" : ""}`}
+            onClick={(event) => onCompleteTask(task.id)}
+          >
+            {checked}
+          </div>
+
+          <input
+            type="text"
+            id="description"
+            className={`description ${task.completed ? "text--checked" : ""}`}
+            name="description"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <div onClick={(event) => event.stopPropagation()}>
+            <button
+              className="delete"
+              onClick={() => {
+                onDeleteTask(task.id);
+              }}
+            >
+              <img src={crossIcon} className="delete-icon" alt="Delete task." />
+            </button>
+          </div>
+        </div>
+      );
+    };
+
+    return renderTask();
+  }
+}
 
 Task.propTypes = {
   /** Composition of the task */
-  id: PropTypes.shape({
+  task: PropTypes.shape({
     // Id of the task
     id: PropTypes.string.isRequired,
 
@@ -78,10 +99,16 @@ Task.propTypes = {
 
   /**  Handler for clicking the Cross icon */
   onDeleteTask: PropTypes.func,
+
+  /**  Handler when the input is changed */
+  onChangeDescription: PropTypes.func,
 };
 
 Task.defaultProps = {
   onClick: undefined,
   onCompleteTask: undefined,
   onDeleteTask: undefined,
+  onChangeDescription: undefined,
 };
+
+export { Task };
