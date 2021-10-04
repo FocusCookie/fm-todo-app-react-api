553061fd27705ccf1119e0b3b00c0cf829a3cc1f
FocusCookie <stephan_lueddemann@web.de>
2021-10-04 09:16:32 +0200

    Added components listining in storybook main

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,12 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/compontents/**/*/*.stories.@(js|jsx|ts|tsx|md)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+    "@storybook/preset-create-react-app",
+  ],
+};
